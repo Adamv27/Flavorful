7d4253a9a1a4037a458ad112fe15721da92bbf65
Adam Vinch <adamvinch@gmail.com>
2023-10-18 02:22:51 -0400

    Get api key from .env. Copy .env file in docker

--- backend/app/backend.py
+++ backend/app/backend.py
@@ -1,8 +1,20 @@
+import os
+import requests
+from dotenv import load_dotenv
 from fastapi import FastAPI
+
+
+load_dotenv()
+
+API_KEY = os.getenv('SPOONACULAR_API_KEY')
+API_KEY_QUERY = f"apiKey={API_KEY}"
+BASE_URL = 'https://api.spoonacular.com'
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/recipes")
 async def root():
-    return {"message": "Hello World!"}
+    url = f'{BASE_URL}/recipes/complexSearch?{API_KEY_QUERY}&cuisine=italian'
+    response = requests.get(url)
+    return response.json()
