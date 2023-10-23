1142c459ee825d7b1bc8d7740e2e068238468e97
Adam Vinch <adamvinch@gmail.com>
2023-10-23 19:56:33 -0400

    Add different views for recipe searching options

--- frontend/script/recipeFinder.js
+++ frontend/script/recipeFinder.js
@@ -5,7 +5,6 @@ const toggleSaveRecipe = (recipeID) => {
     let recipeCard = document.getElementById(recipeID);
     let saveButton = recipeCard.querySelector(".save-button"); 
     saveButton.classList.toggle("saved");
-    console.log(saveButton);
 }
 
 
@@ -33,4 +32,36 @@ const searchForRecipes = async () => {
 }
 
 
-searchForRecipes().then((data) => displayRecipes(data.results))
+//searchForRecipes().then((data) => displayRecipes(data.results))
+
+const toggleOptionViews = (prevID, newID) => {
+    let prevOptionView = document.getElementById(prevID);
+    prevOptionView.classList.remove('current');
+    let newOptionView = document.getElementById(newID);
+    newOptionView.classList.add('current');
+}
+
+
+let currentOptionIndex = 0;
+let optionIDs = ["time-option", "calories-option", "cuisine-option"];
+
+const nextOption = () => {
+    if (currentOptionIndex + 1 < optionIDs.length) {
+        let prevID = optionIDs[currentOptionIndex];
+        currentOptionIndex += 1;
+        let newID = optionIDs[currentOptionIndex];
+        toggleOptionViews(prevID, newID)
+    }
+}
+
+const prevOption = () => {
+    if (currentOptionIndex - 1 >= 0) {
+        let prevID = optionIDs[currentOptionIndex];
+        currentOptionIndex -= 1;
+        let newID = optionIDs[currentOptionIndex];
+        toggleOptionViews(prevID, newID);
+    }
+}
+
+
+
