f092b59b36350ffd7a9a3b893898168655e90f21
Adam Vinch <adamvinch@gmail.com>
2023-11-06 23:32:49 -0500

    Fetch from backend. Add access control to response header in server.

--- script/login.js
+++ script/login.js
@@ -1,27 +1,43 @@
-const form = document.getElementById("login-form")
+const login_form = document.getElementById("login-form")
+const signup_form = document.getElementById("signup-form")
+
+BASE_URL = "http://127.0.01:8000/auth/"
 
 
-const login = async (formData) => {
-    let response = await fetch("http://127.0.0.1:8000/auth/test", {
-        method: "POST",
-        body: formData,
+const signup = async () => {
+    const username = document.getElementById("username-input").value;
+    const password = document.getElementById("password-input").value;
+    const confirm_password = document.getElementById("confirm-password-input").value;
+    const url = BASE_URL + `register?username=${username}&password=${password}&confirm_password=${confirm_password}`
+    
+    await fetch(url, {
+        method: "POST"
     })
-    return response.json()
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+const login = async () => {
+    const username = document.getElementById("username-input").value;
+    const password = document.getElementById("password-input").value;
+    
+    const url = BASE_URL + `username=${username}&password=${password}` 
+    await fetch(url, {
+        method: "POST",
+        mode: "no-cors",
+
+    })
 }
 
 
 window.addEventListener("load", () => {
-    form.addEventListener("submit", (e) => {
+    login_form?.addEventListener("submit", (e) => {
         e.preventDefault();
-
-        const formData = new FormData(); 
-        const username = document.getElementById("username-input").value;
-        const password = document.getElementById("password-input").value;
-        formData.append("username", username)
-        formData.append("password", password)
-        formData.append("grant_type", "password")
-        console.log(formData)
-
-        login(formData)
+        login()
     });
+
+    signup_form?.addEventListener("submit", (e) => {
+        e.preventDefault();
+        signup()
+    })
 });
