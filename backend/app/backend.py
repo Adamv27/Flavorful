b7ef476b8ef4d3f46bfce11ec8be3781bc5b8d93
Adam Vinch <adamvinch@gmail.com>
2023-11-28 22:18:48 -0500

    Add cors middleware to backend

--- backend/app/backend.py
+++ backend/app/backend.py
@@ -5,11 +5,21 @@ from app.config import engine
 from app.routers import recipes
 from app.routers import users
 from app.exception_handlers import add_global_exception_handler
-
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(recipes.router)
 app.include_router(users.router)
