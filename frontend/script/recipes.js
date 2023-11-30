3179d20d5aa9a20aaf1b09a14dc4d36d25d61220
Adam Vinch <adamvinch@gmail.com>
2023-11-30 12:02:26 -0500

    If access token is in session storage access saved recipes on page load

---
+++ frontend/script/recipes.js
@@ -0,0 +1,24 @@
+import { user } from "../script/user.js"
+
+const BASE_URL = "http://127.0.0.1:8000/recipes"
+
+window.onload = () => {
+    const token = user.get_token()
+    if (token) {
+        get_user_recipes(token)
+    }
+}
+
+
+const get_user_recipes = async (token) => {
+    if (!token) return
+
+     fetch(BASE_URL + "/saved", { 
+        method: "GET",
+        headers: {
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`},
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
--- frontend/script/user.js
+++ frontend/script/user.js
@@ -1,9 +1,12 @@
 
 export const user = {
-    token: "",
-
     set_token(new_token) {
-        this.token = new_token;
-    } 
+        console.log("setting token to: " + new_token)
+        sessionStorage.setItem('access_token', new_token)
+    },
+
+    get_token() {
+        return sessionStorage.getItem("access_token") || null;
+    }
 }
 
