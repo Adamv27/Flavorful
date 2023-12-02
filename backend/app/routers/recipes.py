5ec9cead0855dcf80ccd6f460d88cb92573f70be
Adam Vinch <adamvinch@gmail.com>
2023-12-02 18:44:38 -0500

    If recipe already exists in the database do not attempt to save it again

--- backend/app/routers/recipes.py
+++ backend/app/routers/recipes.py
@@ -8,7 +8,9 @@ from app.schemas import RequestRecipe, Response, UserSchema
 from app import database
 from app.database import get_db
 from app.login import get_current_user
+import logging
 
+logger = logging.getLogger('uvicorn')
 
 load_dotenv()
 
@@ -29,8 +31,15 @@ async def create(
         request: RequestRecipe,
         db: Annotated[Session, Depends(get_db)]
 ):
+
     user_id = current_user.username + current_user.hashed_password
+    recipe = database.get_recipe_by_id(db, user_id, request.recipe.id)
+    if (recipe):
+        logger.info(f"{recipe.title} is already in the database for user {current_user.username}")
+        return Response(code=400, status="Ok", message="Recipe already exists!").dict(exclude_none=True)
+
     database.create_recipe(db, recipe=request.recipe, user_id=user_id)
+    logger.info(f"Added {request.recipe.title} for user {current_user.username}")
     return Response(code=200, status="Ok", message="Recipe created successfully").dict(exclude_none=True)
 
 
