8babbbf86bab87e5cf4a63570d6a5417fd234dae
Adam Vinch <adamvinch@gmail.com>
2023-12-02 19:20:38 -0500

    Add remove route to backend to remove a recipe by its IID from the database

--- backend/app/routers/recipes.py
+++ backend/app/routers/recipes.py
@@ -36,17 +36,31 @@ async def create(
     recipe = database.get_recipe_by_id(db, user_id, request.recipe.id)
     if (recipe):
         logger.info(f"{recipe.title} is already in the database for user {current_user.username}")
-        return Response(code=400, status="Ok", message="Recipe already exists!").dict(exclude_none=True)
+        return Response(code=409, status="Conflict", message=f"Recipe already exists for user {current_user.username}").dict(exclude_none=True)
 
     database.create_recipe(db, recipe=request.recipe, user_id=user_id)
     logger.info(f"Added {request.recipe.title} for user {current_user.username}")
-    return Response(code=200, status="Ok", message="Recipe created successfully").dict(exclude_none=True)
+    return Response(code=201, status="Ok", message="Recipe created successfully").dict(exclude_none=True)
+
+
+@router.get("/remove/{recipe_id}")
+async def remove_recipe(
+            recipe_id: int,
+            current_user: Annotated[UserSchema, Depends(get_current_user)],
+            db: Annotated[Session, Depends(get_db)]
+):
+    user_id = current_user.username + current_user.hashed_password
+    if database.remove_recipe(db, user_id, recipe_id):
+        return Response(code=200, status="Ok", message="Removed: " + str(recipe_id)).dict(exclude_none=True)
+
+    return Response(code=400, status="Conflict", message="Failed to remove recipe from databse")
+
 
 
 @router.get("/saved")
 async def saved_recipes(
         current_user: Annotated[UserSchema, Depends(get_current_user)],
-        db: Session = Depends(get_db)
+        db: Session = Depends(get_db),
 ):
     user_id = current_user.username + current_user.hashed_password
     recipes = database.get_recipes_by_user_id(db, user_id)
