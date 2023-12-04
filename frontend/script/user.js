8040fd055a57bc40207edea9f20db8839cf53509
Adam Vinch <adamvinch@gmail.com>
2023-12-04 01:11:18 -0500

    Cache recipe details on successful request. Attempt to retrieve them from localStorage to build the modal before fetching

--- frontend/script/user.js
+++ frontend/script/user.js
@@ -7,6 +7,16 @@ export const user = {
 
     get_token() {
         return sessionStorage.getItem("access_token") || null;
+    },
+
+    cache_recipe_details(recipe_id, details) {
+        const recipe = localStorage.getItem(recipe_id)
+        if (recipe == null)
+            localStorage.setItem(recipe_id, JSON.stringify(details))
+    },
+
+    get_recipe_from_cache(recipe_id) {
+        return JSON.parse(localStorage.getItem(recipe_id))
     }
 }
 
