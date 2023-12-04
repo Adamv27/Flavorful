ea21175118968f95c2e766e58a6f34dcf9c291d8
Adam Vinch <adamvinch@gmail.com>
2023-12-04 11:51:05 -0500

    Fetch saved recipes on DOM content loaded

--- frontend/script/savedrecipes.js
+++ frontend/script/savedrecipes.js
@@ -4,17 +4,17 @@ import { BASE_URL } from "./settings.js"
 
 const URL = BASE_URL + "/recipes" 
 
-window.onload = () => {
+
+const load_recipes = async () => {
     const token = user.get_token()
-    if (token) {
-        get_user_recipes(token)
-    }
+    if (token)
+        await get_user_recipes(token)
 }
 
 
 const get_user_recipes = async (token) => {
     if (!token) return
-
+        
      fetch(URL + "/saved", { 
         method: "GET",
         headers: {
@@ -29,3 +29,5 @@ const get_user_recipes = async (token) => {
         }
     });
 }
+
+document.addEventListener("DOMContentLoaded", load_recipes, false);
